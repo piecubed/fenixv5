dafe11f2605f46902ffc9e08bd69e5c652b88b27
piecubed <40523150+piecubed@users.noreply.github.com>
2020-04-19 20:23:43 -0400

    Make things yay again

--- tests/databasetest.py
+++ tests/databasetest.py
@@ -1,27 +1,26 @@
 import unittest
 from asyncTests import asyncTest
-from fenix.database import Users, User, Servers, Server
+from fenix.database import Database, User, Server
 
 class UserDatabaseTests(unittest.TestCase):
     
-    usersDatabase: Users = Users()
-    serversDatabase: Servers = Servers()
+    database: Database = Database()
     async def createDatabase(self) -> None:
         with open('tables.psql', 'r') as f:
-            await self.usersDatabase._execute(f.read())
+            await self.database._execute(f.read())
     
     @asyncTest #type: ignore
     async def test1(self) -> None:
         await self.createDatabase()
-        await self.usersDatabase.signUp('test', 'testpassword', 'piesquared@gmail.com')
+        await self.database.signUp('test', 'testpassword', 'piesquared@gmail.com')
     
     @asyncTest #type: ignore
     async def test2(self) -> None:
-        await self.usersDatabase.fetchUserByEmail('piesquared@gmail.com')
+        await self.database.fetchUserByEmail('piesquared@gmail.com')
     
     @asyncTest #type: ignore
     async def test3(self) -> None:
-        self.user = await self.usersDatabase.signIn('piesquared@gmail.com', 'testpassword')
+        self.user = await self.database.signIn('piesquared@gmail.com', 'testpassword')
     
     @asyncTest #type: ignore
     async def test4(self) -> None:
@@ -29,7 +28,7 @@ class UserDatabaseTests(unittest.TestCase):
     
     @asyncTest #type: ignore
     async def test5(self) -> None:
-        await self.usersDatabase.joinServer('1', '1')
+        await self.database.joinServer('1', '1')
         await self.createDatabase()
         
 if __name__ == '__main__':
